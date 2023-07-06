b0dee93c0ee15640eed717f05360a3aaf673f3cb
Arun Joseph <arun.kadavunkal@accenture.com>
2023-07-06 23:01:15 +0300

    Added: Unit tests for FaqOverViewViewModel, MockJsonfiles added, MockService added, Extension added for loading json from bundle

--- Assignment/Utils/FetchStatus.swift
+++ Assignment/Utils/FetchStatus.swift
@@ -7,8 +7,18 @@
 
 import Foundation
 
-enum FetchStatus {
+enum FetchStatus: Equatable {
     case processing
     case success
     case error(String)
+    
+    static func == (lhs: FetchStatus, rhs: FetchStatus) -> Bool {
+        switch (lhs, rhs) {
+        case (.success, .success): return true
+        case (.processing, .processing): return true
+        case (.error, .error): return true
+        default: return false
+            
+        }
+    }
 }
---
+++ AssignmentTests/FaqOverViewTests/FaqOverViewViewModelTests.swift
@@ -0,0 +1,73 @@
+//
+//  FaqOverViewViewModelTests.swift
+//  AssignmentTests
+//
+//  Created by arun.joseph on 06/07/2023.
+//
+
+import Foundation
+import XCTest
+import Combine
+import Service
+@testable import Assignment
+
+final class FaqOverViewViewModelTestsL: XCTestCase {
+    
+    var viewModel: FaqOverViewModelType!
+    var cancellables = Set<AnyCancellable>()
+    
+    func testFetchFaqs() async throws {
+        viewModel = FaqOverViewViewModel(faqService: FaqServiceMock())
+        
+        await viewModel.fetchFaqs()
+        
+        XCTAssertEqual(viewModel.faqs.count, 2)
+        viewModel.fetchStatusPublisher.sink { fetchStatus in
+            XCTAssertEqual(fetchStatus, .success)
+        }.store(in: &cancellables)
+    }
+    
+    func testFetchFailed() async throws {
+        let serviceMock = FaqServiceMock()
+        serviceMock.isFailed = true
+        viewModel = FaqOverViewViewModel(faqService: serviceMock)
+        
+
+        await viewModel.fetchFaqs()
+        
+        XCTAssertEqual(viewModel.faqs.count, 0)
+        viewModel.fetchStatusPublisher.sink { fetchStatus in
+            XCTAssertEqual(fetchStatus, .error("error while fetching"))
+        }.store(in: &cancellables)
+    }
+    
+    func testIsTitleReturnsTrue() async throws {
+        viewModel = FaqOverViewViewModel(faqService: FaqServiceMock())
+
+        await viewModel.fetchFaqs()
+        
+        let faqElements = try JSONDecoder().decode([RemoteFaqElement].self, from: Data(forResource: "MockElement"))
+        let isTitle = viewModel.isTitle(faqElements[0])
+        
+        XCTAssertTrue(isTitle)
+    }
+    
+    func testIsTitleReturnsFalse() async throws {
+        viewModel = FaqOverViewViewModel(faqService: FaqServiceMock())
+
+        await viewModel.fetchFaqs()
+        
+        let faqElements = try JSONDecoder().decode([RemoteFaqElement].self, from: Data(forResource: "MockElement"))
+        let isTitle = viewModel.isTitle(faqElements[1])
+        
+        XCTAssertFalse(isTitle)
+    }
+    
+    func testgetFaqElements() async throws {
+        viewModel = FaqOverViewViewModel(faqService: FaqServiceMock())
+        
+        await viewModel.fetchFaqs()
+        
+        XCTAssertEqual(viewModel.faqElements.count, 7)
+    }
+}
