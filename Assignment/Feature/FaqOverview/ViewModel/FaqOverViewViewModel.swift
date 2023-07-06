90f39055a182487c4b3bb1003b64278c950e50dd
Arun Joseph <arun.kadavunkal@accenture.com>
2023-07-06 21:56:07 +0300

    Add: FaqOverView Fetch functionality completed,  FaqOverView UI updated with faqs fetched

--- Assignment/Feature/FaqOverview/ViewModel/FaqOverViewViewModel.swift
+++ Assignment/Feature/FaqOverview/ViewModel/FaqOverViewViewModel.swift
@@ -7,28 +7,51 @@
 
 import Foundation
 import Service
+import Combine
 
 protocol FaqOverViewModelType {
-    var faqElements: [RemoteFaq] { get }
-    var error: String? { get }
     func fetchFaqs() async
+    var fetchStatusPublisher: Published<FetchStatus>.Publisher { get }
+    var faqs: [RemoteFaq] { get }
+    var faqElements: [RemoteFaqElement] { get }
+    func isTitle(_ faqElement: RemoteFaqElement) -> Bool
 }
 
 final class FaqOverViewViewModel: FaqOverViewModelType {
 
     let faqService: FaqServiceProtocol
-    @Published var faqElements: [RemoteFaq] = []
-    @Published var error: String?
+    var faqs: [RemoteFaq] = []
+    var faqElements: [RemoteFaqElement] = []
+    @Published var fetchStatus: FetchStatus = .processing
+    var fetchStatusPublisher: Published<FetchStatus>.Publisher { $fetchStatus }
 
     init(faqService: FaqServiceProtocol = FaqService()) {
         self.faqService = faqService
     }
 
+    @MainActor
     func fetchFaqs() async {
+        self.fetchStatus = .processing
         do {
-            self.faqElements = try await faqService.fetch()
+            self.faqs = try await faqService.fetch()
+            self.getFaqElements()
+            self.fetchStatus = .success
         } catch {
-            self.error = error.localizedDescription
+            self.fetchStatus = .error(error.localizedDescription)
+        }
+    }
+
+    // MARK: is type of title
+    func isTitle(_ faqElement: RemoteFaqElement) -> Bool {
+        return faqElement.type == "title"
+    }
+
+    // MARK: get faq elements
+    func getFaqElements() {
+        for faq in faqs {
+            for element in faq.elements {
+                faqElements.append(element)
+            }
         }
     }
 }
---
+++ Assignment/Utils/FetchStatus.swift
@@ -0,0 +1,14 @@
+//
+//  FetchStatus.swift
+//  Assignment
+//
+//  Created by arun.joseph on 06/07/2023.
+//
+
+import Foundation
+
+enum FetchStatus {
+    case processing
+    case success
+    case error(String)
+}
